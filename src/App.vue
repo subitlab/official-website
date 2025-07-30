eba5242b8d69bd58b6ec7e6a4d7d34707c03b24e
LittleSwift <60036604+LittleSwift@users.noreply.github.com>
2025-07-30 17:56:45 +0800

    feat: vue-router setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <nav>
+    <RouterLink to="/"><!--TODO SubIT logo-->SubIT</RouterLink>
+    <RouterLink to="/about">关于我们</RouterLink>
+    <RouterLink to="/join">加入我们</RouterLink>
+    <RouterLink to="/achievements">项目成就</RouterLink>
+    <RouterLink to="/support">提供支持</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import '@/style.css';
+import App from '@/App.vue';
+import {createRouter, createWebHistory} from "vue-router";
+import IndexView from "@/routes/IndexView.vue";
+import AboutView from "@/routes/AboutView.vue";
+import JoinView from "@/routes/JoinView.vue";
+import AchievementsView from "@/routes/AchievementsView.vue";
+import SupportView from "@/routes/SupportView.vue";
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: IndexView },
+  { path: '/about', component: AboutView },
+  { path: '/join', component: JoinView },
+  { path: '/achievements', component: AchievementsView },
+  { path: '/support', component: SupportView }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
