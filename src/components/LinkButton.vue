5df1624d7dbc72d071b8f95c4e1db2c5e9009d31
LittleSwift <60036604+LittleSwift@users.noreply.github.com>
2025-08-01 02:42:18 +0800

    feat: support view

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const { href } = defineProps<{
+  href: string
+}>();
+
+function openInNewTab() {
+  window.open(href, '_blank');
+}
+</script>
+
+<template>
+  <button class="linkButton" @click="openInNewTab">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped lang="scss">
+.linkButton {
+  height: 3em;
+  padding: 0.5em 1em;
+  border-radius: 0.5em;
+
+  border: none;
+
+  background-color: #3D99F5;
+  color: white;
+
+  cursor: pointer;
+
+  font-weight: 700;
+}
+</style>
--- src/routes/SupportView.vue
+++ src/routes/SupportView.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 
+import Content from "@/components/Content.vue";
+import LinkButton from "@/components/LinkButton.vue";
 </script>
 
 <template>
-
+  <Content>
+    <h1>技术支持</h1>
+    <h2>本地支持</h2>
+    <p>在工作日的午餐休息时间和活动时段内，可提供离线技术支持服务。请提前通过发送电子邮件的方式预订您的服务时段。</p>
+    <h2>在线支持</h2>
+    <p>如需在线技术支持，请直接向 SubIT 发送电子邮件。在邮件中请清晰地描述您的问题。主要的沟通方式将是通过电子邮件进行。</p>
+    <LinkButton href="mailto:contact@subit.org.cn">Email SubIT</LinkButton>
+  </Content>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
